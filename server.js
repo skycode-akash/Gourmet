82e4ce92875acd89f0a4f3a97a369cb833c07fab
Akash Bhavsar <akashbhavsar1510@gmail.com>
2021-10-07 22:18:45 +0530

    'prob-mongoose'

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose')
+
+//database connection
+const url = 'mongodb://localhost:27017/Gourmet'
+// mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true});
+// const connection = mongoose.connection;
+// mongoose.connection.once('open', () => {
+// console.log('database connected...');
+// }).catch(err => {
+//     console.log('connection failed...');
+// })
 
 // Assets
 app.use(express.static('public'))
@@ -13,7 +24,7 @@ app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
-require('./routes/web') (app)
+require('./routes/web')(app)
 
 app.listen(PORT, () => {
     console.log(`Listing on port ${PORT}`)
