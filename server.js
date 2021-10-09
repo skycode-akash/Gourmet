a3503addf9c8c89441848659f64f4e52c0f344cc
Akash Bhavsar <akashbhavsar1510@gmail.com>
2021-10-09 19:21:46 +0530

    'cart-session'

--- server.js
+++ server.js
@@ -85,6 +85,12 @@ app.use(flash())
 app.use(express.static('public'))
 app.use(express.json())
 
+//global middleware
+app.use((req, res, next) => {
+    res.locals.session = req.session
+    next()
+})
+
 // set template engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
